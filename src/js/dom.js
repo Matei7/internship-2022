f69fc3b4e79de24f942591b156bcd5ba07ec0248
Matei <vmatei967@gmail.com>
2022-07-10 18:24:57 +0300

    dom exercises

---
+++ src/js/dom.js
@@ -0,0 +1,16 @@
+//  Part I
+//  1. Get the main content container
+//  2. Get first post title
+//  3. Get first post content
+//  4. Get all post titles
+//  5. Change the value for the first title
+//  6. Change the URL for the first title link
+//  7. Change the background color for the body
+//  8. Add a new class to the articles then add styles
+
+//  Part II
+//  1.  Select the parent element for the first post title
+//  2.  Select the first post and log the siblings
+//  3.  Select the #main container and log the children
+
+// Bonus Set the entry content with the current date and time and update it every x (as parameter) seconds
