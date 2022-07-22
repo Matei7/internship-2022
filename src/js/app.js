a7c551b73e3b80666d025b3de14e9e941311d5c2
Andreea Maior <andreea.maior@bitstone.eu>
2022-07-22 15:34:45 +0300

    added ex 1 and 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,37 @@
 import "../css/styles.scss";
 
-console.log("hello world!");
+let a, b, c, product;
+a = 5;
+b = 6;
+c = -7;
+product = a * b * c;
+if (product >= 0)
+{
+    console.log("PLUS");
+}
+else
+{
+    console.log("MINUS");
+}
 
-
-
+var dict = {
+    10: "pen",
+    200: "cat",
+    3000: "dog",
+    40000: "horse",
+    500000: "car"
+};
+const weight = prompt("What's the weight?");
+let found = "false";
+for(let value in dict)
+{
+    if (value == weight)
+    {
+        console.log(dict[value]);
+        found = "true";
+    }
+}
+if (found == "false")
+{
+    console.log("A truckload of bunnies");
+}
