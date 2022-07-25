a36251084caba38a08785ac08077666ef12a8c5d
Mihai Tudor <mihai.tudor@bitstone.eu>
2022-07-25 14:01:45 +0300

    finished Part1 and Part2

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,151 @@
 import "../css/styles.scss";
 
-console.log("hello world!");
+///objects
 
+var person = {
+    age: 22,
+    firstName: "Mihai",
+    lastName: "Tudor"
+};
 
+///functions
 
+function justCheckingTheSign(n1, n2, n3){
+    if ((n1 > 0 && n2 > 0 && n3 > 0) ||
+        (n1 > 0 && n2 < 0 && n3 < 0) ||
+        (n1 < 0 && n2 > 0 && n3 < 0) ||
+        (n1 < 0 && n2 < 0 && n3 > 0)) {
+        return "PLUS";
+    }
+    else{
+        return "MINUS";
+    }
+    ///este o functie sign care returneaza 1/0/-1 si in functie de ea sa fac verificare
+}
+
+console.log(justCheckingTheSign(1,2,3));
+console.log(justCheckingTheSign(1,2,-3));
+console.log(justCheckingTheSign(1,-2,-3));
+console.log(justCheckingTheSign(-1,-2,-3));
+
+///b.-ul ii la partea de arrays
+
+///classes - ES5
+function Human(fullName){
+    this.fullName = fullName;
+    this.callMe = function(){
+        console.log("HI I'M " + this.fullName);
+    };
+}
+
+var myInstance = new Human("Tudor Mihai");
+myInstance.callMe();
+
+///arrays
+//a.
+const numbers = []
+for(let i = 0; i < 50; ++i){
+    numbers.push(i);
+}
+
+numbers = [...numbers, 89, 99, 120, 412, 124]
+
+for(let i = 0; i < numbers.length; ++i){
+    console.log(numbers[i]);
+}
+
+//b.
+console.log(numbers[numbers.length - 1]);
+numbers.pop();
+console.log(numbers[numbers.length - 1]);
+
+//5 b.
+function notDivisible(theArray){
+    theArray.forEach(element => {
+        if (element % 3 !== 0)
+            console.log(element)
+    });
+}
+
+notDivisible(numbers);
+
+///loops
+//a.
+function sum(n){
+    console.log(n*(n + 1) / 2);
+}
+
+sum(1000);
+
+//b.
+function multiply(n){
+    while (n < 10000){
+        console.log(n * Math.random() * 10);
+        n++;
+    }
+}
+
+multiply(9996);
+multiply(10000);
+
+///conditionals
+//a.
+var n1 = -4;
+var n2 = -5;
+var n3 = 2;
+
+if ((n1 > 0 && n2 > 0 && n3 > 0) ||
+    (n1 > 0 && n2 < 0 && n3 < 0) ||
+    (n1 < 0 && n2 > 0 && n3 < 0) ||
+    (n1 < 0 && n2 < 0 && n3 > 0)) {
+    console.log("PLUS");
+}
+else{
+    console.log("MINUS");
+}
+
+//b.
+
+function recalculate(a ,b){
+    return a * b;
+}
+
+function aGuess(theWeight){
+    var a = 10;
+    var i = 1;
+    if (theWeight == recalculate(a,i)){
+        console.log("A pen");
+        return;
+    }
+    a *= 10;
+    i++;
+    if (theWeight == recalculate(a,i)){
+        console.log("A cat");
+        return;
+    }
+    a *= 10;
+    i++;
+    if (theWeight == recalculate(a,i)){
+        console.log("A dog");
+        return;
+    }
+    a *= 10;
+    i++;
+    if (theWeight == recalculate(a, i)){
+        console.log("A horse");
+        return;
+    }
+    a *= 100;
+    i++;
+    if (theWeight == recalculate(a, i)){
+        console.log("A car");
+        return;
+    }
+    console.log("A truckload of bunnies");
+}
+
+///facut cu switch si retinut valorile dintr-o lista
+
+aGuess(10);
+aGuess(421);
+aGuess(5000000);
--- src/js/dom.js
+++ src/js/dom.js
@@ -1,16 +1,43 @@
 //  Part I
 //  1. Get the main content container
+let mainContent = document.getElementById('main');
 //  2. Get first post title
+let firstPostTitle = document.querySelector('.post .entry-title').innerText;
 //  3. Get first post content
+let firstPostContent = document.querySelector('.post .entry-content').innerText;
 //  4. Get all post titles
+let allPostTitle = [];
+for (let element of document.querySelectorAll('.post .entry-title')){
+    allPostTitle.push(element.innerText);
+}
 //  5. Change the value for the first title
+document.querySelector('.post .entry-title a').innerText = 'Just Title 1'
 //  6. Change the URL for the first title link
+let firstTitleLink = document.getElementById("gg");
+firstTitleLink.setAttribute("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 //  7. Change the background color for the body
+document.body.style.setProperty('background-color', '#1991C5');
 //  8. Add a new class to the articles then add styles
+document.querySelectorAll('article').forEach(element =>
+    {
+        element.classList.add('my_style_post');
+        element.style.setProperty('color', '#1A485C');
+        element.style.setProperty('padding', '15px');
+    })
 
 //  Part II
 //  1.  Select the parent element for the first post title
+document.querySelector('.post .entry-title').parentElement;
 //  2.  Select the first post and log the siblings
+console.log("Fratii primului post: ");
+let theKidsOfPost = document.querySelector('.post').parentElement.children;
+for(let i = 0; i < theKidsOfPost.length; ++i){
+    console.log(theKidsOfPost[i]);
+}
 //  3.  Select the #main container and log the children
-
+console.log("Copii lui main container: ")
+let kidsOfMain = document.querySelector('#main').children;
+for (let i = 0; i < kidsOfMain.length; ++i){
+    console.log(kidsOfMain[i])
+}
 // Bonus Set the entry content with the current date and time and update it every x (as parameter) seconds
