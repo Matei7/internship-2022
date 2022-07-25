f5b27e9d012a3c6972614de7452a88b50ddb9308
Razvan <razvan.codrescu@bitstone.eu>
2022-07-25 13:53:27 +0300

    rezolvat primele exercitii inainte de tabla de sah

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,69 @@
-import "../css/styles.scss";
+function sumOfThree(number1, number2, number3){
+    return(Math.sign(number1 * number2 * number3) < 0 ? 'MINUS' : 'PLUS');
+}
 
-console.log("hello world!");
+//de terminat
+function secretSurprise(){
+    let aPen = 10, aCat = 200, aDog = 3000, aHorse = 40000, aCar = 5000000;
+
+    let chosenObject = aPen;
+
+    for(let i = 0; i < 5; i++){
+        console.log("Take a guess: ");
 
 
 
+        console.log(input == chosenObject ? "Yes, you got it!" : "No, take another guess");
+    }
+}
+
+function sumForFirstNNumbers(n){
+    let sum = 0;
+    for(let i = 1; i <= n; i++)
+        sum += i;
+
+    return sum;
+}
+
+function randomNumbers(n){
+    while(n < 10000.0){
+        n = n * Math.random() * 10;
+        console.log("Number: " + n);
+    }
+}
+
+function createArray(){
+    const numbers = [];
+    for(let i = 1; i <= 50; i++)
+        numbers.push(i);
+
+    numbers.push(89, 99, 120, 412, 124);
+
+    console.log(numbers.pop());
+
+    for(let i = 0; i < numbers.length; i++)
+        console.log(numbers.at(i) % 3 === 0 ? numbers.at(i) : "");
+}
+
+const object = {age: 31, firstName: "Razvan", secondName: "Codrescu"};
+
+class Human{
+    #fullName;
+
+    constructor(fullName){
+        this.#fullName = fullName;
+    }
+
+    method(){
+        console.log("HI I'M " + this.#fullName);
+    }
+
+}
+
+let human = new Human("Razvan");
+human.method();
+
+//console.log(createArray());
+//console.log(randomNumbers(13));
+//console.log(sumForFirstNNumbers(1000));
+//console.log(sumOfThree(1, 2, -1));
--- src/js/dom.js
+++ src/js/dom.js
@@ -14,3 +14,90 @@
 //  3.  Select the #main container and log the children
 
 // Bonus Set the entry content with the current date and time and update it every x (as parameter) seconds
+
+function partIMethod() {
+    const container = document.getElementById('main');
+    console.log(container);
+
+    const firstPostTitle = document.getElementById('gg');
+    console.log("First post title: " + firstPostTitle.textContent);
+
+    const firstPostContent = document.querySelector('.entry-content p').textContent;
+    console.log("First post content: " + firstPostContent);
+
+    const allPostTitles = document.querySelectorAll('.entry-title a');
+    allPostTitles.forEach(function (entryPost) {
+        console.log(entryPost.textContent);
+    });
+
+    document.querySelector('.entry-title a').innerText = 'New Title';
+
+    document.querySelector('.entry-title a').href = 'index.html';
+
+    document.body.style.backgroundColor = 'lightgreen';
+
+    allPosts = document.querySelectorAll('article');
+    let inc = 1;
+
+
+    allPosts.forEach(function (entryPost) {
+
+        entryPost.className += 'secondClass';
+        entryPost.style.fontFamily = 'Arial';
+
+        //Initial credeam ca trebuie adaugat un element nou
+        const newParagraph = document.createElement('p');
+        newParagraph.style.color = 'darkorange';
+        newParagraph.style.fontWeight = 'bolder';
+        newParagraph.style.fontSize = '20px';
+        newParagraph.innerText = 'New Paragraph ' + inc;
+
+        inc++;
+
+        entryPost.appendChild(newParagraph);
+    })
+
+}
+
+function partIIMethod(){
+    //1.
+    const firstPostParent = document.querySelector('.entry-title').parentElement;
+    console.log(firstPostParent);
+
+    //2.
+    const firstPostSibling = document.querySelector('article').nextElementSibling;
+    const nextPostSibling = firstPostSibling.nextElementSibling;
+    console.log(firstPostSibling.innerText);
+    console.log(nextPostSibling.innerText);
+
+    //3.
+    console.log("Body's children: ");
+    const main = document.getElementById('main');
+    for(let i = 0; i < main.children.length; i++){
+        console.log(main.children[i].tagName);
+    }
+
+}
+
+function getDateTime(x){
+    const today = new Date();
+
+    let date = today.getDay() + "-" + today.getMonth() + "-" + today.getFullYear();
+
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    const newParagraph = document.createElement('p');
+    newParagraph.innerText = date + " " + time;
+    document.body.appendChild(newParagraph);
+
+    window.setInterval('refresh()', x);
+
+}
+
+function refresh() {
+    window.location.reload();
+}
+
+console.log(getDateTime(3000));
+console.log(partIMethod());
+console.log(partIIMethod());
