3da1ad63fcd0a017c308abf23518898c48dcb2a9
Gabriela Torzsa <gabriela.torzsa@bitstone.eu>
2022-07-27 13:54:42 +0300

    table done

--- src/js/table.js
+++ src/js/table.js
@@ -59,15 +59,88 @@ function drawTable() {
 }
 
 function initialState() {
+    //primele 5 din casa
     const firstFiveWhiteDiv = document.createElement("div")
     firstFiveWhiteDiv.classList.add("first-five-white");
     document.getElementById("triangleDownEven_0").appendChild(firstFiveWhiteDiv);
 
+    const firstFiveBlackDiv = document.createElement("div")
+    firstFiveBlackDiv.classList.add("first-five-black");
+    document.getElementById("triangleEven_0").appendChild(firstFiveBlackDiv);
+
     for(let i = 0; i < 5; i++) {
-        const circleDiv = document.createElement("div");
-        circleDiv.classList.add("white-circles");
-        circleDiv.setAttribute("id", "circle_" + i);
-        firstFiveWhiteDiv.appendChild(circleDiv);
+        const circleWhiteDiv = document.createElement("div");
+        circleWhiteDiv.classList.add("white-circles");
+        circleWhiteDiv.setAttribute("id", "circleWhite_" + i);
+        firstFiveWhiteDiv.appendChild(circleWhiteDiv);
+
+        const circleBlackDiv = document.createElement('div');
+        circleBlackDiv.classList.add("black-circles");
+        circleBlackDiv.setAttribute("id", "circleBlack_" + i);
+        firstFiveBlackDiv.appendChild(circleBlackDiv);
+    }
+
+    // cele 2 piese din casa
+    const twoPiecesWhiteDiv = document.createElement("div")
+    twoPiecesWhiteDiv.classList.add("two-white");
+    document.getElementById("triangleEven_5").appendChild(twoPiecesWhiteDiv);
+
+    const twoPiecesBlackDiv = document.createElement("div")
+    twoPiecesBlackDiv.classList.add("two-black");
+    document.getElementById("triangleDownEven_5").appendChild(twoPiecesBlackDiv);
+
+    for(let i = 0; i < 2; i++) {
+        const circleWhiteDiv = document.createElement("div");
+        circleWhiteDiv.classList.add("white-circles");
+        circleWhiteDiv.setAttribute("id", "circleWhite_" + i);
+        twoPiecesWhiteDiv.appendChild(circleWhiteDiv);
+
+        const circleBlackDiv = document.createElement('div');
+        circleBlackDiv.classList.add("black-circles");
+        circleBlackDiv.setAttribute("id", "circleBlack_" + i);
+        twoPiecesBlackDiv.appendChild(circleBlackDiv);
+    }
+
+    // cele 5 din afara casei
+    const outerFivePiecesWhiteDiv = document.createElement("div")
+    outerFivePiecesWhiteDiv.classList.add("five-white");
+    document.getElementById("triangleOdd_0").appendChild(outerFivePiecesWhiteDiv);
+
+    const outerFivePiecesBlackDiv = document.createElement("div")
+    outerFivePiecesBlackDiv.classList.add("five-black");
+    document.getElementById("triangleDownOdd_0").appendChild(outerFivePiecesBlackDiv);
+
+    for(let i = 0; i < 5; i++) {
+        const circleWhiteDiv = document.createElement("div");
+        circleWhiteDiv.classList.add("white-circles");
+        circleWhiteDiv.setAttribute("id", "circleWhite_" + i);
+        outerFivePiecesWhiteDiv.appendChild(circleWhiteDiv);
+
+        const circleBlackDiv = document.createElement('div');
+        circleBlackDiv.classList.add("black-circles");
+        circleBlackDiv.setAttribute("id", "circleBlack_" + i);
+        outerFivePiecesBlackDiv.appendChild(circleBlackDiv);
+    }
+
+    // cele 3 din afara casei
+    const outerThreePiecesWhiteDiv = document.createElement("div")
+    outerThreePiecesWhiteDiv.classList.add("three-white");
+    document.getElementById("triangleDownOdd_4").appendChild(outerThreePiecesWhiteDiv);
+
+    const outerThreePiecesBlackDiv = document.createElement("div")
+    outerThreePiecesBlackDiv.classList.add("three-black");
+    document.getElementById("triangleOdd_4").appendChild(outerThreePiecesBlackDiv);
+
+    for(let i = 0; i < 3; i++) {
+        const circleWhiteDiv = document.createElement("div");
+        circleWhiteDiv.classList.add("white-circles");
+        circleWhiteDiv.setAttribute("id", "circleWhite_" + i);
+        outerThreePiecesWhiteDiv.appendChild(circleWhiteDiv);
+
+        const circleBlackDiv = document.createElement('div');
+        circleBlackDiv.classList.add("black-circles");
+        circleBlackDiv.setAttribute("id", "circleBlack_" + i);
+        outerThreePiecesBlackDiv.appendChild(circleBlackDiv);
     }
 }
 
