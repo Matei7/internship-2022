43f5818b61e7d5d85ccf0aee5d9c6695ef276d72
Mihaela Petricele <mihaela.petricele@bitstone.eu>
2022-07-29 14:26:31 +0300

    done Part I in "jQuery.js"

--- src/js/jQuery.js
+++ src/js/jQuery.js
@@ -222,10 +222,384 @@ $("#Ex20").append("<button id='right'>&gt;</button>");
 $("#Ex20").append("<div class='block'></div>");
 
 $( "#right" ).click(function() {
-    $( ".block" ).animate({ "left": "+=50px" }, "slow" );
+    $( "#Ex20 .block" ).animate({ "left": "+=50px" }, "slow" );
 });
 
 $( "#left" ).click(function(){
-    $( ".block" ).animate({ "left": "-=50px" }, "slow" );
+    $( "#Ex20 .block" ).animate({ "left": "-=50px" }, "slow" );
 });
+
+    //Exercitiul 21
+
+$('<div>',{
+    id:'Ex21',
+}).appendTo("body");
+$("#Ex21").append("<h2>Exercitiul 21</h2>");
+$("#Ex21").append("<div class=\"block\"></div");
+$("#Ex21").append("<div class=\"block\"></div>");
+$("#Ex21").append("<div class=\"block\"></div>");
+$("#Ex21").append("<div class=\"block\"></div>");
+
+$("#Ex21").append("<button id='button10'>Click to Run >></button>");
+
+$("#button10").click(function(){
+    $("#Ex21 .block:first").animate({
+        left:100
+    },{
+        duration:2000,
+        step: function (now){
+            $("#Ex21 .block").css("left",now);
+        }
+    });
+});
+
+    //Exercitiul 22
+
+$('<div>',{
+    id:'Ex22',
+}).appendTo("body");
+$("#Ex22").append("<h2>Exercitiul 22</h2>");
+$("#Ex22").append("<div id=\"div1\"></div>");
+$("#Ex22").append("<div id=\"div2\"></div>");
+$("#Ex22").append("<div id=\"div3\"></div>");
+$("#Ex22").append("<button id='button11'>Click to see the effect</button>");
+
+$("#button11").click(function (){
+        $("#Ex22 div:animated").toggleClass("colored");
+});
+function animateIt(){
+    $("#Ex22 #div1,#Ex22 #div2").slideToggle("slow",animateIt);
+}
+animateIt();
+
+    // Exercitiul 23
+
+$('<div>',{
+    id:'Ex23',
+}).appendTo("body");
+$("#Ex23").append("<h2>Exercitiul 23</h2>");
+$("#Ex23").append("<p>JavaScript</p>");
+$("#Ex23").append("<p>jQuery</p>");
+$("#Ex23").append("<button id='button12'>Click to see the changes</button>");
+
+$("#button12").click(function (){
+    $("#Ex23 p ").append(document.createTextNode("  Exercises"));
+});
+
+    // Exercitiul 24
+
+$('<div>',{
+    id:'Ex24',
+}).appendTo("body");
+$("#Ex24").append("<h2>Exercitiul 24</h2>");
+$("#Ex24").append("<strong style='margin-left: 20px;'> Exercises</strong>");
+$("#Ex24").append("<p>JavaScript</p>");
+$("#Ex24").append("<p>jQuery</p>");
+$("#Ex24").append("<button id='button13'>Click to see the changes</button>");
+
+$("#button13").click(function (){
+    $("#Ex24 p ").append($("#Ex24 strong"));
+});
+
+    // Exercitiul 25
+
+$('<div>',{
+    id:'Ex25',
+}).appendTo("body");
+$("#Ex25").append("<h2>Exercitiul 25</h2>");
+$("#Ex25").append("<p>JavaScript</p>");
+$("#Ex25").append("<p>jQuery</p>");
+$("#Ex25").append("<button id='button14'>Click to see the changes</button>");
+
+$("#button14").click(function (){
+    $("#Ex25 p ").append("  Exercises");
+});
+
+    // Exercitiul 26
+
+$('<div>',{
+    id:'Ex26',
+}).appendTo("body");
+$("#Ex26").append("<h2>Exercitiul 26</h2>");
+$("#Ex26").append("<span> Exercises  </span>");
+$("#Ex26").append("<p  id=\"myid\">jQuery</p>");
+$("#Ex26").append("<button id='button15'>Click to see the changes</button>");
+
+$("#button15").click(function (){
+    //$("#Ex26 p ").append($("#Ex26 span")); // var1
+    $("#Ex26 span ").appendTo("#myid"); // var2
+});
+
+    // Exercitiul 27
+
+$('<div>',{
+    id:'Ex27',
+}).appendTo("body");
+$("#Ex27").append("<h2>Exercitiul 27</h2>");
+$("#Ex27").append("<input id=\"check1\" type=\"checkbox\" checked=\"checked\"> <label for=\"check1\">Check me</label>");
+
+$("input").change(function (){
+    let $input = $(this);
+    console.log(".attr('checked')");
+    console.log(".prop('checked'): " + $input.prop("checked"));
+    console.log(".is(':checked' ): " + $input.is(":checked"));
+})
+    .change();
+
+    // Exercitiul 28
+
+$('<div>',{
+    id:'Ex28',
+}).appendTo("body");
+$("#Ex28").append("<h2>Exercitiul 28</h2>");
+$("#Ex28").append("<p>jQuery Exercises, Practice, <em title=\"Real life jQuery or all.\">Solution</em>.</p> <p id=\"id1\"></p>");
+$("#Ex28").append("<button id='button16'>Click to see the effect</button>");
+
+$("#button16").click(function (){
+   let firstEmTitle = $("em").attr("title");
+   $("#id1").text(firstEmTitle);
+});
+
+    // Exercitiul 29
+
+$('<div>',{
+    id:'Ex29',
+}).appendTo("body");
+$("#Ex29").append("<h2>Exercitiul 29</h2>");
+$("#Ex29").append("<p><a href=\"w3resource.com\" hreflang=\"es\">href language es</a></p> " +
+                    "<p><a href=\"w3resource.com\" hreflang=\"de\">href language de</a></p>" +
+                    "<p><a href=\"w3resource.com\" hreflang=\"en\">href language en</a></p>");
+$("#Ex29").append("<button id='button17'>Click to see the effect</button>");
+
+$("#button17").click(function (){
+    $("a[hreflang|='de']").css("border","2px solid red");
+});
+
+    // Exercitiul 30
+
+$('<div>',{
+    id:'Ex30',
+}).appendTo("body");
+$("#Ex30").append("<h2>Exercitiul 30</h2>");
+$("#Ex30").append("<div name=\"tutorial-php\">" +
+                "<p>PHP</p>" +
+                "</div>" +
+                "<div name=\"java-articles\"><p>Java</p>" +
+                "</div>" +
+                "<div name=\"python-tutorial-and-exercises\"> <p>Python</p>" +
+                "</div>");
+$("#Ex30").append("<button id='button18'>Click to see the effect</button>");
+
+$("#button18").click(function (){
+    $("#Ex30 div[name*='tutorial']").css("background","yellow");
+});
+
+    // Exercitiul 31
+
+$('<div>',{
+    id:'Ex31',
+}).appendTo("body");
+$("#Ex31").append("<h2>Exercitiul 31</h2>");
+$("#Ex31").append("<div name=\"tutorial-php\">" +
+    "<p>PHP</p>" +
+    "</div>" +
+    "<div name=\"java tutorial\"><p>Java</p>" +
+    "</div>" +
+    "<div name=\"python-tutorial-and-exercises\"> <p>Python</p>" +
+    "</div>");
+$("#Ex31").append("<button id='button19'>Click to see the effect</button>");
+
+$("#button19").click(function (){
+    $( "div[name~='tutorial']" ).css( "background", "yellow" );
+});
+
+    // Exercitiul 32
+
+$('<div>',{
+    id:'Ex32',
+}).appendTo("body");
+$("#Ex32").append("<h2>Exercitiul 32</h2>");
+$("#Ex32").append("<div  name='tutorial-php'>" +
+                    "<p>PHP</p>" +
+                    "</div>" +
+                    "<div  name='JAVAtutorial' <p style='margin-left: 20px;'>Java</p>" +
+                    "</div>" +
+                    "<div  name='python-tutorial'>" +
+                    "<p>Python</p>" +
+                    "</div>");
+$("#Ex32").append("<button id='button20'>Click to see the effect</button>");
+
+$("#button20").click(function (){
+    $( "#Ex32 div[name$='tutorial']" ).css( "background", "yellow" );
+});
+
+    // Exercitiul 33
+
+$('<div>',{
+    id:'Ex33',
+}).appendTo("body");
+$("#Ex33").append("<h2>Exercitiul 33</h2>");
+$("#Ex33").append("<div>" +
+                    "<label>" +
+                    "<input type='radio' name='color' value='Red'>" +
+                    "<span>name?</span>" +
+                    "</label>" +
+                    "</div>" +
+                    "<div>" +
+                    "<label>" +
+                    "<input type='radio' name='color' value='Green'>" +
+                    "<span>value?</span>" +
+                    "</label>" +
+                    "</div>" +
+                    "<div>" +
+                    "<label>" +
+                    "<input type='radio' name='color' value='Black'>" +
+                    "<span>value?</span>" +
+                    "</label>" +
+                    "</div>");
+$("#Ex33").append("<button id='button21'>Click to see the effect</button>");
+
+$("#button21").click(function (){
+    $( "#Ex33 input[value='Red']" ).next().text( " Red " );
+});
+
+    // Exercitiul 34
+
+$('<div>',{
+    id:'Ex34',
+}).appendTo("body");
+$("#Ex34").append("<h2>Exercitiul 34</h2>");
+$("#Ex34").append("<input type='radio' name='color'  value='Red'>" +
+                    "<span>Red</span>" +
+                    "</div>" +
+                    "<div>" +
+                    "<input type='radio' value='Cold Fusion'>" +
+                    "<span>Sky</span>" +
+                    "</div>" +
+                    "<div>" +
+                    "<input type='radio' name='accept'  value='Evil Plans'>" +
+                    "<span>Sea</span>" +
+                    "</div>");
+$("#Ex34").append("<button id='button22'>Click to see the effect</button>");
+
+$("#button22").click(function (){
+    $( "#Ex34 input[name!='color']").next().append("<b>: not a color</b>");
+});
+
+    // Exercitiul 35
+
+$('<div>',{
+    id:'Ex35',
+}).appendTo("body");
+$("#Ex35").append("<h2>Exercitiul 35</h2>");
+$("#Ex35").append("<input name='PHP'> <input name='Python'> <input name='Redis'>");
+$("#Ex35").append("<button id='button23'>Click to see the effect</button>");
+
+$("#button23").click(function (){
+    $( "#Ex35 input[name^='P']").val("Tutorials & Exercises");
+});
+
+    // Exercitiul 36
+
+$('<div>',{
+    id:'Ex36',
+}).appendTo("body");
+$("#Ex36").append("<h2>Exercitiul 36</h2>");
+$("#Ex36").append("<p>PHP Tutorial</p>\n" +
+                    "<p>Python Tutorial</p>\n" +
+                    "<p>Java Tutorial</p>");
+$("#Ex36").append("<button id='button24'>Click to see the effect</button>");
+
+$("#button24").click(function (){
+    $( "#Ex36 p" ).before( "<i style='margin-left: 20px;'>some HTML</i>" );
+});
+
+    // Exercitiul 37
+
+$('<div>',{
+    id:'Ex37',
+}).appendTo("body");
+$("#Ex37").append("<h2>Exercitiul 37</h2>");
+$("#Ex37").append("<p>PHP Tutorial</p>\n" +
+                    "<p>Python Tutorial</p>\n" +
+                    "<p>Java Tutorial</p>");
+$("#Ex37").append("<button id='button25'>Click to see the effect</button>");
+
+$("#button25").click(function (){
+    $( "#Ex37 p" ).before(document.createTextNode("DOM element")) ;
+});
+
+    // Exercitiul 38
+
+$('<div>',{
+    id:'Ex38',
+}).appendTo("body");
+$("#Ex38").append("<h2>Exercitiul 38</h2>");
+$("#Ex38").append("<p>PHP Tutorial</p><b>w3resource</b>");
+$("#Ex38").append("<button id='button26'>Click to see the effect</button>");
+
+$("#button26").click(function (){
+    $( "#Ex38 p" ).before($("b")) ;
+});
+
+    // Exercitiul 39
+
+$('<div>',{
+    id:'Ex39',
+}).appendTo("body");
+$("#Ex39").append("<h2>Exercitiul 39</h2>");
+$("#Ex39").append("<p>Click or double click here.</p>\n" +
+                    " <p id=\"result\"></p>");
+
+$("#Ex39 p").bind("click",function (event){
+        let s = "(" + event.pageX + "," + event.pageY + ")";
+        $("#result").text("Click happened!" + s);
+});
+
+$("#Ex39 p").bind("dblclick", function (){
+    $( "#result" ).text( "Double-click happened");
+});
+
+$( "#Ex39 #result" ).bind( "mouseenter mouseleave", function() {
+    $( this ).toggleClass( "over" );
+});
+
+    // Exercitiul 40
+
+$('<div>',{
+    id:'Ex40',
+}).appendTo("body");
+$("#Ex40").append("<h2>Exercitiul 40</h2>");
+$("#Ex40").append("<form>\n" +
+                    "<input id=\"target\" type=\"text\" value=\"Field 1\">\n" +
+                    "<input type=\"text\" value=\"Field 2\">\n" +
+                    "</form>\n" +
+                    "<p>Set and remove focus from field1\n" +
+                    "</p>");
+
+$( "#target" ).blur(function() {
+    console.log( "Remove focus from fields1!" );
+});
+
+    // Exercitiul 41
+
+$('<div>',{
+    id:'Ex41',
+}).appendTo("body");
+$("#Ex41").append("<h2>Exercitiul 41</h2>");
+$("#Ex41").append("<form>\n" +
+                " <fieldset>\n" +
+                " <textarea rows=\"4\" cols=\"50\">\n" +
+                " At w3resource.com you will learn how to make a website. We offer free tutorials in all web development technologies. \n" +
+                " </textarea>\n" +
+                " <input type=\"button\" value=\"Input Button\">\n" +
+                " </fieldset>\n" +
+                " </form>\n" +
+                " <p></p>");
+$("#Ex41").append("<button id='button27'>Click to see the effect</button>");
+$("#button27").click(function (){
+    let inp = $("#Ex41 :button").addClass("marked");
+    $("#Ex41 p").text("Nr. of buttons(s): " + inp.length);
+});
+
 
