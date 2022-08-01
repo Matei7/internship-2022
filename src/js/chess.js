945467403c61d01dc0d78fe4630c22908745ad6d
Mihaela Petricele <mihaela.petricele@bitstone.eu>
2022-08-01 11:12:44 +0300

    change in method movePieces in "chess.js"

--- src/js/chess.js
+++ src/js/chess.js
@@ -1,19 +1,27 @@
 import "../css/style-chess.scss";
-
+import $ from "jquery";
 
 let table;
 let count = document.createElement("div");
 count.classList.add("count");
-let piecesPosition = {
-    "a8":"rookB","b8":"knightB","c8":"bishopB","d8":"queenB","e8":"kingB","f8":"bishopB","g8":"knight8","h8":"rookB",
-    "a7":"pawnB","b7":"pawnB","c7":"pawnB","d7":"pawnB","e7":"pawnB","f7":"pawnB","g7":"pawnB","h7":"pawnB",
-    "a6":"none","b6":"none","c6":"none","d6":"none","e6":"none","f6":"none","g6":"none","h6":"none",
-    "a5":"none","b5":"none","c5":"none","d5":"none","e5":"none","f5":"none","g5":"none","h5":"none",
-    "a4":"none","b4":"none","c4":"none","d4":"none","e4":"none","f4":"none","g4":"none","h4":"none",
-    "a3":"none","b3":"none","c3":"none","d3":"none","e3":"none","f3":"none","g3":"none","h3":"none",
-    "a2":"pawnW","b2":"pawnW","c2":"pawnW","d2":"pawnW","e2":"pawnW","f2":"pawnW","g2":"pawnW","h2":"pawnW",
-    "a1":"rookW","b1":"knightW","c1":"bishopW","d1":"queenW","e1":"kingW","f1":"bishopW","g1":"knightW","h1":"rookW"
+let piecesPosition = { "a8": "rookB", "b8": "knightB", "c8": "bishopB", "d8": "queenB", "e8": "kingB", "f8": "bishopB", "g8": "knightB", "h8": "rookB",
+                       "a7": "pawnB", "b7": "pawnB", "c7": "pawnB", "d7": "pawnB", "e7": "pawnB", "f7": "pawnB", "g7": "pawnB", "h7": "pawnB",
+                       "a6": "none", "b6": "none", "c6": "none", "d6": "none", "e6": "none", "f6": "none", "g6": "none", "h6": "none",
+                       "a5": "none", "b5": "none", "c5": "none", "d5": "none", "e5": "none", "f5": "none", "g5": "none", "h5": "none",
+                       "a4": "none", "b4": "none", "c4": "none", "d4": "none", "e4": "none", "f4": "none", "g4": "none", "h4": "none",
+                       "a3": "none", "b3": "none", "c3": "none", "d3": "none", "e3": "none", "f3": "none", "g3": "none", "h3": "none",
+                       "a2": "pawnW", "b2": "pawnW", "c2": "pawnW", "d2": "pawnW", "e2": "pawnW", "f2": "pawnW","g2": "pawnW", "h2": "pawnW",
+                       "a1": "rookW", "b1": "knightW", "c1": "bishopW", "d1": "queenW", "e1": "kingW", "f1": "bishopW", "g1": "knightW", "h1": "rookW"
 }
+
+let position = [['a8','b8','c8','d8','e8','f8','g8','h8'],
+                ['a7','b7','c7','d7','e7','f7','g7','h7'],
+                ['a6','b6','c6','d6','e6','f6','g6','h6'],
+                ['a5','b5','c5','d5','e5','f5','g5','h5'],
+                ['a4','b4','c4','d4','e4','f4','g4','h4'],
+                ['a3','b3','c3','d3','e3','f3','g3','h1'],
+                ['a2','b2','c2','d2','e2','f2','g2','h2'],
+                ['a1','b1','c1','d1','e1','f1','g1','h3']];
 
 function board() {
     table = document.createElement("div");
@@ -48,160 +56,38 @@ function board() {
 
 }
 
-function addButton(){
+function startGame() {
+    for (let piece in piecesPosition) {
+        let p = piecesPosition[piece];
+        let pieceId = document.getElementById(piece);
+        if (piecesPosition[piece].includes("B")) {
+            pieceId.classList.add(p.substring(0, p.indexOf('B')) + "-black");
+            pieceId.classList.add("piece");
+        } else {
+            if (piecesPosition[piece].includes("W")) {
+                pieceId.classList.add(p.substring(0, p.indexOf('W')) + "-white");
+                pieceId.classList.add("piece");
+            }
+        }
+    }
+}
+function addButton() {
     let divButton = document.createElement("div");
     divButton.classList.add("divButton");
     let button = document.createElement("button");
     button.innerHTML = "Start new game";
     divButton.appendChild(button);
     document.body.appendChild(divButton);
-    button.addEventListener("click",function (){
+    button.addEventListener("click", function () {
         board();
         startGame();
         movePieces();
     });
 }
 
-function startGame(){
-
-    // black pieces
-    let rookB = document.getElementById("a8");
-    rookB.classList.add("rook-black");
-    rookB.classList.add("piece");
-
-    let knightB = document.getElementById("b8");
-    knightB.classList.add("knight-black");
-    knightB.classList.add("piece");
-
-    let bishopB = document.getElementById("c8");
-    bishopB.classList.add("bishop-black");
-    bishopB.classList.add("piece");
-
-    let queenB = document.getElementById("d8");
-    queenB.classList.add("queen-black");
-    queenB.classList.add("piece");
-
-    let kingB = document.getElementById("e8");
-    kingB.classList.add("king-black");
-    kingB.classList.add("piece");
-
-    let bishopB1 = document.getElementById("f8");
-    bishopB1.classList.add("bishop-black");
-    bishopB1.classList.add("piece");
-
-    let knightB1 = document.getElementById("g8");
-    knightB1.classList.add("knight-black");
-    knightB1.classList.add("piece");
-
-    let rookB1 = document.getElementById("h8");
-    rookB1.classList.add("rook-black");
-    rookB1.classList.add("piece");
-
-    let pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8;
-    pawn1 = document.getElementById("a7");
-    pawn1.classList.add("pawn-black");
-    pawn1.classList.add("piece");
-
-    pawn2 = document.getElementById("b7");
-    pawn2.classList.add("pawn-black");
-    pawn2.classList.add("piece");
-
-    pawn3 = document.getElementById("c7");
-    pawn3.classList.add("pawn-black");
-    pawn3.classList.add("piece");
-
-    pawn4 = document.getElementById("d7");
-    pawn4.classList.add("pawn-black");
-    pawn4.classList.add("piece");
-
-    pawn5 = document.getElementById("e7");
-    pawn5.classList.add("pawn-black");
-    pawn5.classList.add("piece");
-
-    pawn6 = document.getElementById("f7");
-    pawn6.classList.add("pawn-black");
-    pawn6.classList.add("piece");
-
-    pawn7 = document.getElementById("g7");
-    pawn7.classList.add("pawn-black");
-    pawn7.classList.add("piece");
-
-    pawn8 = document.getElementById("h7");
-    pawn8.classList.add("pawn-black");
-    pawn8.classList.add("piece");
-
-    //white pieces
-    let rookW = document.getElementById("a1");
-    rookW.classList.add("rook-white");
-    rookW.classList.add("piece");
-
-    let knightW = document.getElementById("b1");
-    knightW.classList.add("knight-white");
-    knightW.classList.add("piece");
-
-    let bishopW = document.getElementById("c1");
-    bishopW.classList.add("bishop-white");
-    bishopW.classList.add("piece");
-
-    let queenW = document.getElementById("d1");
-    queenW.classList.add("queen-white");
-    queenW.classList.add("piece");
-
-    let kingW = document.getElementById("e1");
-    kingW.classList.add("king-white");
-    kingW.classList.add("piece");
-
-    let bishopW1 = document.getElementById("f1");
-    bishopW1.classList.add("bishop-white");
-    bishopW1.classList.add("piece");
-
-    let knightW1 = document.getElementById("g1");
-    knightW1.classList.add("knight-white");
-    knightW1.classList.add("piece");
-
-    let rookW1 = document.getElementById("h1");
-    rookW1.classList.add("rook-white");
-    rookW1.classList.add("piece");
-
-    let pawn1W, pawn2W, pawn3W, pawn4W, pawn5W, pawn6W, pawn7W, pawn8W;
-    pawn1W = document.getElementById("a2");
-    pawn1W.classList.add("pawn-white");
-    pawn1W.classList.add("piece");
-
-    pawn2W = document.getElementById("b2");
-    pawn2W.classList.add("pawn-white");
-    pawn2W.classList.add("piece");
-
-    pawn3W = document.getElementById("c2");
-    pawn3W.classList.add("pawn-white");
-    pawn3W.classList.add("piece");
-
-    pawn4W = document.getElementById("d2");
-    pawn4W.classList.add("pawn-white");
-    pawn4W.classList.add("piece");
-
-    pawn5W = document.getElementById("e2");
-    pawn5W.classList.add("pawn-white");
-    pawn5W.classList.add("piece");
-
-    pawn6W = document.getElementById("f2");
-    pawn6W.classList.add("pawn-white");
-    pawn6W.classList.add("piece");
-
-    pawn7W = document.getElementById("g2");
-    pawn7W.classList.add("pawn-white");
-    pawn7W.classList.add("piece");
-
-    pawn8W = document.getElementById("h2");
-    pawn8W.classList.add("pawn-white");
-    pawn8W.classList.add("piece");
-
-}
-
-
 // count.innerHTML = "The game will start in";
 // document.body.appendChild(count);
-let seconds = 5;
+// let seconds = 5;
 // let countDown = setInterval(function(){
 //     if(seconds > 0){
 //         count.innerHTML = seconds;
@@ -216,33 +102,71 @@ let seconds = 5;
 
 addButton();
 
-function movePieces(){
+function movePieces() {
 
-    for(let i = 0; i < 64; i++){
-        let cellSelected = document.querySelectorAll(".piece")[i];
+    let cellSelected = document.getElementsByClassName("piece");
+    for (let i = 0; i < cellSelected.length; i++) {
+        // let cellSelected = document.querySelectorAll(".piece")[i];
 
-        cellSelected.addEventListener("mouseover",function (){
-            cellSelected.classList.add("selector");
+        cellSelected[i].addEventListener("mouseover",function (){
+            cellSelected[i].classList.add("selector");
         });
-        cellSelected.addEventListener("mouseout",function (){
-            cellSelected.classList.remove("selector");
+        cellSelected[i].addEventListener("mouseout",function (){
+            cellSelected[i].classList.remove("selector");
         });
 
 
+        cellSelected[i].addEventListener("click", () => {
+            let check = document.querySelector(".selector");
+            if (check !== undefined) {
+                let idSelected = cellSelected[i].id;
+                for (let piece in piecesPosition) {
+                    if(idSelected === piece){
+                        if(piece === "a7"){
+                            $(".highlight").toggleClass("highlight");
+                            $(document.getElementById("a5")).toggleClass("highlight");
+                            $(document.getElementById("a6")).toggleClass("highlight");
 
+                        }
+                        else{
+                            if(piece === "b7"){
+                                $(".highlight").toggleClass("highlight");
+                                $(document.getElementById("b5")).toggleClass("highlight");
+                                $(document.getElementById("b6")).toggleClass("highlight");
+                            }
+                            else{
+                                if(piece === "b8"){
+                                    $(".highlight").toggleClass("highlight");
+                                    $(document.getElementById("a6")).toggleClass("highlight");
+                                    $(document.getElementById("c6")).toggleClass("highlight");
+                                }
+                                else{
+                                    if(piece === "a2"){
+                                        $(".highlight").toggleClass("highlight");
+                                        $(document.getElementById("a3")).toggleClass("highlight");
+                                        $(document.getElementById("a4")).toggleClass("highlight");
+                                    } else {
+                                        if (piece === "b2") {
+                                            $(".highlight").toggleClass("highlight");
+                                            $(document.getElementById("b3")).toggleClass("highlight");
+                                            $(document.getElementById("b4")).toggleClass("highlight");
+                                        }
+                                        else{
+                                            if(piece === "b1"){
+                                                $(".highlight").toggleClass("highlight");
+                                                $(document.getElementById("a3")).toggleClass("highlight");
+                                                $(document.getElementById("c3")).toggleClass("highlight");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
-
-        cellSelected.addEventListener("click",()=>{
-            let check= document.querySelector(".selector");
-            if(check !== undefined){
-                let idSelected = cellSelected.id;
-                document.getElementById("a4").style.backgroundColor = "wheat";
-                document.getElementById("a5").style.backgroundColor = "wheat";
-                document.getElementById("a6").style.backgroundColor = "wheat";
             }
         })
-
-
     }
 
 
